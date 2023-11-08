19438858e29c3da30726a03b4376f92c3ceec7ff
Zach <user@Users-MacBook-Pro.local>
2023-11-08 13:52:48 -0800

    first

--- src/components/ServiceHeader.tsx
+++ src/components/ServiceHeader.tsx
@@ -2,31 +2,37 @@ import { styled } from "styled-components";
 import Button from "../common/Button";
 
 const ServiceHeader = () => {
-  return (<Container>
-    <Title>On-Demand help to make you the best offer.</Title>
-    <Description>We are always avalible to answer any questions or concers you may have. If youre lights stop working, dont know how to navigate the app, or if you just have any general questions we are here to help. </Description>
-    <Button height="40px">Request a quote</Button>
-  </Container>);
+  return (<Section>
+    <Container>
+      <Title>On-Demand help to make you the best offer.</Title>
+      <Description>We are always avalible to answer any questions or concers you may have. If youre lights stop working, dont know how to navigate the app, or if you just have any general questions we are here to help. </Description>
+      <Button height="40px">Request a quote</Button>
+    </Container>
+  </Section>
+  );
 }
 
 export default ServiceHeader;
 
+
+const Section = styled.section`
+  display: flex;
+  justify-content: center;
+`
 const Container = styled.div`
   height: 45vh;
-  width: 100vw;
+  width: 480px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 `
 
 const Title = styled.h2`
   font-size: 40px;
-  max-width: 475px;
-
 `
 
 const Description = styled.p`
-  max-width: 450px;
   font-size: 20px;
   
 `
