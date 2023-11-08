1dbddc32e8410da8ab20158cfab57a23baa80878
Zach <user@Users-MacBook-Pro.local>
2023-11-08 14:00:03 -0800

    firsst

--- src/components/ServiceHeader.tsx
+++ src/components/ServiceHeader.tsx
@@ -1,39 +1,51 @@
-import { styled } from "styled-components";
+import React from "react";
+import styled from "styled-components";
 import Button from "../common/Button";
 
 const ServiceHeader = () => {
-  return (<Section>
-    <Container>
-      <Title>On-Demand help to make you the best offer.</Title>
-      <Description>We are always avalible to answer any questions or concers you may have. If youre lights stop working, dont know how to navigate the app, or if you just have any general questions we are here to help. </Description>
-      <Button height="40px">Request a quote</Button>
-    </Container>
-  </Section>
+  return (
+    <Section>
+      <Container>
+        <Title>On-Demand Help to Make You the Best Offer</Title>
+        <Description>
+          We are always available to answer any questions or concerns you may
+          have. If your lights stop working, don't know how to navigate the
+          app, or if you just have any general questions, we are here to help.
+        </Description>
+        <ButtonWrapper>
+          <Button height="40px">Request a Quote</Button>
+        </ButtonWrapper>
+      </Container>
+    </Section>
   );
-}
+};
 
 export default ServiceHeader;
-
 
 const Section = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+  height: 50vh;
+`;
+
 const Container = styled.div`
-  min-height: 50vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
+  text-align: center;
+  max-width: 600px;
+  padding: 20px;
+`;
 
 const Title = styled.h2`
-  font-size: 40px;
-  max-width: 470px;
-`
+  font-size: 32px;
+  margin-bottom: 20px;
+`;
 
 const Description = styled.p`
-  font-size: 20px;
-  max-width: 480px;
-`
+  font-size: 18px;
+  margin-bottom: 30px;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 20px;
+`;
+
