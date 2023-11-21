8929df5b298d9a317fda22ac16958455b2084682
System Administrator <root@Users-MacBook-Pro.local>
2023-11-20 16:47:18 -0800

    done

--- src/common/Modal.jsx
+++ src/common/Modal.jsx
@@ -27,7 +27,7 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: white;
+  background-color: #ffffff;
   border-radius: 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   border: 1px solid #ccc;
